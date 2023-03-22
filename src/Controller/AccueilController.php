967e2423e993a4a96494414100c14e71e4b513af
bigno <bigno@THE-BEAST>
2023-03-22 11:38:52 +0100

    Phase 2: premier test 'helloworld

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * 
+ */
+class AccueilController {
+    /**
+     * @Route("/", name="acceuil")
+     * @return Response
+     */
+    public function index() : Response {
+        return new Response('hello world !');
+    }
+}
