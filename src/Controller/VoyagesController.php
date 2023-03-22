f7016a1e4e8f00b0e74bebac9f4089671104621a
bigno <bigno@THE-BEAST>
2023-03-22 12:10:27 +0100

    Phase 4: plusieures pages

---
+++ src/Controller/VoyagesController.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Description of VoyagesController
+ *
+ * @author bigno
+ */
+class VoyagesController extends AbstractController{
+    /**
+     * @Route("/voyages", name="voyages")
+     */
+    public function index() : Response {
+        return $this->render("pages/voyages.html.twig");
+    }
+}
